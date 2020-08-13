6c899207057baacb831e28f2b524e737cfd43fc1
Kishori Mohan Konwar <kishori82@gmail.com>
2020-08-13 11:11:02 -0400

    added the compressed file

---
+++ src/editor/qtextedittooltip.cpp
@@ -0,0 +1,6 @@
+#include "qtextedittooltip.h"
+
+QTextEditToolTip::QTextEditToolTip()
+{
+
+}
---
+++ src/editor/qtextedittooltip.h
@@ -0,0 +1,11 @@
+#ifndef QTEXTEDITTOOLTIP_H
+#define QTEXTEDITTOOLTIP_H
+
+
+class QTextEditToolTip : public QTextEdit
+{
+public:
+    QTextEditToolTip();
+};
+
+#endif // QTEXTEDITTOOLTIP_H
